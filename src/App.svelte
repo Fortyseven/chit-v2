80154b51e5b717b2055aa5ed6e9b8708d72bf367
Fortyseven <fortyseven@network47.org>
2024-12-12 19:39:25 -0500

    feat: sticky chat knobs

--- src/App.svelte
+++ src/App.svelte
@@ -6,67 +6,67 @@
         AppRail,
         AppRailAnchor,
         AppRailTile,
-        AppShell
+        AppShell,
     } from "@skeletonlabs/skeleton"
 
     import ConvoPanel from "$app/Convos/ConvoPanel.svelte"
 
-
     import "$stores/appState.svelte.js"
 
-    import llm from '$lib/llm/ollama.svelte.js'
+    import llm from "$lib/llm/ollama.svelte.js"
     import API from "./app/API.svelte"
     import ChatLogRegular from "./app/Chat/ChatLogRegular.svelte"
     import InputBar from "./app/Chat/InputBar/InputBar.svelte"
     import EventRepeater from "./app/EventRepeater.svelte"
-// import { currentConvoIndex } from "./stores/appState.svelte"
+    // import { currentConvoIndex } from "./stores/appState.svelte"
+    import ChatKnobs from "./app/Chat/ChatKnobs.svelte"
     import { convos, currentConvo } from "./stores/chatState.svelte.js"
-    import ChatKnobs from "./stores/ChatState/ChatKnobs.svelte"
 </script>
 
 <EventRepeater>
     <API>
         <AppShell class="h-full">
             {#snippet header()}
-                <AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end"
-                        background="bg-surface-700"
+                <AppBar
+                    gridColumns="grid-cols-3"
+                    slotDefault="place-self-center"
+                    slotTrail="place-content-end"
+                    background="bg-surface-700"
                 >
                     {#snippet lead()}
                         <!-- <button class="btn variant-filled-secondary">Menu</button> -->
                     {/snippet}
 
-                    <div class="text-lg text-primary-500 font-bold text-nowrap text-ellipsis overflow-hidden w-3/4 md:w-full md:text-2xl">
+                    <div
+                        class="text-lg text-primary-500 font-bold text-nowrap text-ellipsis overflow-hidden w-3/4 md:w-full md:text-2xl"
+                    >
                         {$currentConvo.title}
                     </div>
 
                     {#snippet trail()}
-                    <!-- Bar -->
+                        <!-- Bar -->
                     {/snippet}
                 </AppBar>
             {/snippet}
 
             {#snippet sidebarLeft()}
-                <ConvoPanel/>
+                <ConvoPanel />
             {/snippet}
 
-            {#snippet pageHeader()}
-                <ChatKnobs></ChatKnobs>
-            {/snippet}
-
+            {#snippet pageHeader()}{/snippet}
 
             <div class="page h-full w-full relative">
+                <ChatKnobs></ChatKnobs>
                 <div class="h-full overflow-y-auto flex-col pb-24">
                     <ChatLogRegular></ChatLogRegular>
                 </div>
-                <div class="input-bar" >
+                <div class="input-bar">
                     <InputBar></InputBar>
                 </div>
-
             </div>
         </AppShell>
     </API>
 </EventRepeater>
-
 
 <style lang="scss">
     :global(.app-rail button) {
