fb2e3e34d40d32b0bbb4bd4df33b986f488b0668
Fortyseven <fortyseven@network47.org>
2025-02-12 03:31:11 -0500

    refactor: persisting is broken, so just consoldate these

--- src/stores/appState.svelte.js
+++ src/stores/appState.svelte.js
@@ -2,11 +2,18 @@
 
 import { writable } from "svelte/store"
 
-export const knobLock = $state({
+export const DEFAULT_OL_ENDPOINT = "http://localhost:11434"
+
+export const knobLock = writable({
     knobs: false,
     model: false,
     system: false,
 })
 
-// export const currentChatSessionIndex = writable(0)
 export const selectedPresetIndex = writable(undefined)
+
+export const appState = writable({
+    apiEndpoint: DEFAULT_OL_ENDPOINT,
+    constrainChatWidth: false,
+    useSfx: true,
+})
