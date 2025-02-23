feeffb2433ee52a94c6e9e065ef18416f0586e04
Fortyseven <fortyseven@network47.org>
2025-02-22 23:52:29 -0500

    feat: rewrite of system prompt dialog using daisy

--- src/app/Chat/ChatKnobs/SystemPrompt.svelte
+++ src/app/Chat/ChatKnobs/SystemPrompt.svelte
@@ -1,28 +1,8 @@
 <script>
-    import {
-        arrow,
-        autoUpdate,
-        computePosition,
-        flip,
-        offset,
-        shift,
-    } from "@floating-ui/dom"
-    import { popup, storePopup } from "@skeletonlabs/skeleton"
-    import { writable } from "svelte/store"
+    import { derived, writable } from "svelte/store"
     import { appState } from "../../../appState/appState"
     import { chatSetSystemPrompt } from "../../../chatSession/chatActions"
     import { currentChat } from "../../../chatSession/chatSession"
-
-    storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow })
-
-    const popupSystemPrompt = {
-        // Represents the type of event that opens/closed the popup
-        event: "click",
-        // Matches the data-popup value on your popup element
-        target: "popupSystemPrompt",
-        // Defines which side of your trigger the popup will appear
-        placement: "bottom",
-    }
 
     let sys_prompt_state = writable($currentChat.system_prompt)
 
@@ -36,27 +16,35 @@
             sys_prompt_state.set($currentChat.system_prompt)
         }
     })
+
+    const PREVIEW_CUTOFF_LENGTH = 50
+
+    const shortPrompt = derived(sys_prompt_state, ($sys_prompt_state) => {
+        return $sys_prompt_state.length > PREVIEW_CUTOFF_LENGTH
+            ? $sys_prompt_state.slice(0, PREVIEW_CUTOFF_LENGTH) + "..."
+            : $sys_prompt_state
+    })
 </script>
 
-<div class="flex">
-    <button
-        class="variant-ghost-primary [&>*]:pointer-events-none text-ellipsis text-nowrap overflow-hidden h-11 w-full sm:max-w-xs"
-        use:popup={popupSystemPrompt}
-    >
-        {$sys_prompt_state || "No SPrompt"}
-    </button>
-    <div
-        class="card w-full shadow-2xl flex flex-wrap sm:flex-nowrap gap-4 hidden"
-        data-popup="popupSystemPrompt"
-    >
-        <div class="arrow">&uparrow;</div>
+<button tabindex="0" class="btn m-1" onclick={sprompt.showModal()}>
+    {$shortPrompt || "No SPrompt"}
+</button>
+
+<dialog id="sprompt" class="modal">
+    <div class="modal-box">
+        <h2 class="font-bold text-xl pb-4">System Prompt</h2>
         <textarea
-            class="system-prompt w-full p-2"
-            name="prompt"
             id="prompt"
+            name="prompt"
+            class="w-full"
             placeholder="Write a system prompt..."
             rows="10"
             bind:value={$sys_prompt_state}
         ></textarea>
+        <div class="modal-action">
+            <form method="dialog">
+                <button class="btn btn-primary">Close</button>
+            </form>
+        </div>
     </div>
-</div>
+</dialog>
