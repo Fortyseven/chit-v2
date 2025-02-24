682ba3272e5e18b72ac383594183b6d15d55ff46
Fortyseven <fortyseven@network47.org>
2025-02-24 01:24:14 -0500

    fix: updates targets to fix github build

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,25 @@ import { svelte } from "@sveltejs/vite-plugin-svelte"
 import path from "path"
 import { defineConfig } from "vite"
 
+const IGNORED_WARNINGS = [
+    "a11y-autofocus",
+    "a11y-click-events-have-key-events",
+    "a11y-label-has-associated-control",
+    "a11y-missing-attribute",
+    "a11y-no-noninteractive-element-interactions",
+    "a11y-no-static-element-interactions",
+    "css-unused-selector",
+]
+
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
         svelte({
             compilerOptions: {
                 runes: false,
+            },
+            onwarn(warning, handler) {
+                if (!IGNORED_WARNINGS.includes(warning.code)) handler(warning)
             },
         }),
     ],
