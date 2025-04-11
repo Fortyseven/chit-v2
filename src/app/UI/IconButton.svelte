dd0af3a53a9da73ec43350f21ea9a8fdec265c07
Fortyseven <fortyseven@network47.org>
2025-04-11 07:25:27 -0400

    fix: update IconButton styles for improved layout and accessibility

--- src/app/UI/IconButton.svelte
+++ src/app/UI/IconButton.svelte
@@ -4,30 +4,26 @@
     export let disabled: boolean
     export let iconComponent: any
     export let className: string = ""
+    export let secondary: boolean = false
 </script>
 
 <!-- svelte-ignore a11y_consider_explicit_label -->
-<button
-    onclick={onClick}
-    {disabled}
->
-    <svelte:component this={iconComponent} {title} class="m-auto"></svelte:component>
+<button onclick={onClick} {disabled} class={secondary ? "secondary" : ""}>
+    <svelte:component this={iconComponent} {title}></svelte:component>
 </button>
 
 <style lang="scss">
-
     button {
         padding: 0;
         display: inline-flex;
-        width: 8px;
-        height: 8px;
-        background-color: var(--color-primary-500);
-        color: white;
+        border-radius: 0px;
+        width: 50px;
+        height: 50px;
         place-content: center;
+        align-items: center;
 
         &:disabled {
             opacity: 0.5;
         }
     }
-
 </style>
