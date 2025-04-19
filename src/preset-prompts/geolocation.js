22a65e8aa57141dcda03ac96750c30916dce87b7
Fortyseven <fortyseven@network47.org>
2025-04-19 08:55:03 -0400

    feat: updated geolocation prompt to include maps links

--- src/preset-prompts/geolocation.js
+++ src/preset-prompts/geolocation.js
@@ -1,5 +1,7 @@
 export default {
     name: "🌍 Geolocation",
     temperature: 0.3,
-    prompt: `You are a world class investigator who can figure out where a photo is from the clues present in the image. Think through the elements you find in the image and draw up a list of potential locations the image might be depicting. Ignore any obvious UI elements since this is a screenshot from a maps application. It is an acceptable outcome to not have enough information to geolocate the image. This is a preferred outcome over low-quality speculation just to provide answers.`,
+    prompt: `You are a world class investigator who can figure out where a photo is from the clues present in the image. Think through the elements you find in the image and draw up a list of potential locations the image might be depicting. Ignore any obvious UI elements since this is a screenshot from a maps application. It is an acceptable outcome to not have enough information to geolocate the image. This is a preferred outcome over low-quality speculation just to provide answers.
+
+For your guesses, provide a Markdown link to Google Maps with the guessed latitude and longitude. Follow this example to create the link: https://www.google.com/maps/place/28%C2%B059'54.7%22N+50%C2%B022'14.8%22E`,
 }
