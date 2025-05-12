fe28d9d427d07620cca7a818537f2e7c8dea13ff
Fortyseven <fortyseven@network47.org>
2025-05-12 11:04:36 -0400

    fix: uncentered, unwide status

--- src/app/Chat/InputBar/Status/Status.svelte
+++ src/app/Chat/InputBar/Status/Status.svelte
@@ -5,37 +5,44 @@
 </script>
 
 <div class="status-bar">
-    <div class="left"></div>
-    <div class="center">
-        <Status__Counter {inputLength} />
+    <div class="wrapper">
+        <div class="left"></div>
+        <div class="center">
+            <Status__Counter {inputLength} />
+        </div>
+        <div class="right"></div>
     </div>
-    <div class="right"></div>
 </div>
 
 <style lang="scss">
     .status-bar {
-        display: grid;
-        grid-template-columns: 1fr 1fr 1fr;
         background: linear-gradient(to bottom, transparent 0%, #0004 100%);
         height: 1em;
         padding-block: 0.5em;
         padding-inline: 1em;
-        color: white;
-        max-width: var(--timeline-max-width);
-        .left,
-        .right,
-        .center {
+        .wrapper {
+            max-width: var(--timeline-max-width);
             width: 100%;
-            display: flex;
-        }
-        .left {
-            justify-content: start;
-        }
-        .center {
-            justify-content: center;
-        }
-        .right {
-            justify-content: end;
+            margin: auto;
+            display: grid;
+            grid-template-columns: 1fr 1fr 1fr;
+            color: white;
+
+            .left,
+            .right,
+            .center {
+                width: 100%;
+                display: flex;
+            }
+            .left {
+                justify-content: start;
+            }
+            .center {
+                justify-content: center;
+            }
+            .right {
+                justify-content: end;
+            }
         }
     }
 </style>
