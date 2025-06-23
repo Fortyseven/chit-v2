58260507fd74ee08dd9916ffcd82e21fba9bba52
Fortyseven <fortyseven@network47.org>
2025-06-23 09:44:06 -0400

    feat: renames and replaces art prompter with a more modern one

--- src/preset-prompts/index.js
+++ src/preset-prompts/index.js
@@ -4,13 +4,13 @@ import gtei from "./fun/gtei.js"
 import linux_prompt from "./fun/linux-prompt.js"
 
 import analysis from "./analysis.js"
+import art_prompter from "./art_prompter.js"
 import codealyzer from "./codealyzer.js"
 import eli5 from "./eli5.js"
 import extract_and_infer from "./extract-and-infer.js"
 import general from "./general.js"
 import geolocation from "./geolocation.js"
 import retort from "./retort.js"
-import sdprompt from "./sdprompt.js"
 import summarize from "./summarize.js"
 import summary_meeting from "./summary-meeting.js"
 import translate from "./translate.js"
@@ -23,7 +23,7 @@ export const SYSTEM_PROMPTS = {
     general,
     geolocation,
     retort,
-    sdprompt,
+    art_prompter,
     summarize,
     summary_meeting,
     translate,
