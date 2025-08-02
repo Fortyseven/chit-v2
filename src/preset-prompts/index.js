f4a529bf3d54b1531df706b84384b4f7fc6e3db0
Fortyseven <fortyseven@network47.org>
2025-08-02 16:29:11 -0400

    feat: adds 'idea feedback' prompt

--- src/preset-prompts/index.js
+++ src/preset-prompts/index.js
@@ -10,6 +10,7 @@ import eli5 from "./eli5.js"
 import extract_and_infer from "./extract-and-infer.js"
 import general from "./general.js"
 import geolocation from "./geolocation.js"
+import idea_feedback from "./idea-feedback.js"
 import retort from "./retort.js"
 import summarize from "./summarize.js"
 import summary_meeting from "./summary-meeting.js"
@@ -17,13 +18,14 @@ import translate from "./translate.js"
 
 export const SYSTEM_PROMPTS = {
     analysis,
+    art_prompter,
     codealyzer,
     eli5,
     extract_and_infer,
     general,
     geolocation,
+    idea_feedback,
     retort,
-    art_prompter,
     summarize,
     summary_meeting,
     translate,
