48ad2b2dc1ae81895af81c7910485b7fa44a19c7
Fortyseven <fortyseven@network47.org>
2025-08-02 16:28:55 -0400

    feat: updates dumb prompt

--- src/preset-prompts/fun/appearance-assessment.js
+++ src/preset-prompts/fun/appearance-assessment.js
@@ -1,29 +1,29 @@
 /* NOTE: This is wildly toxic and is just for fun. It is not meant to be taken seriously. Robots have no sense of beauty as we understand it. Probably. */
 
 export default {
-    name: '💋 Fun: Appearance Assessment',
+    name: '💋 Fun: Appear. Ass.',
     temperature: 0.7,
     prompt: `Rate this individual's appearance on various metrics:
 
-    Facial symmetry
-    Golden Ratio/Facial Proportions
-    Hair quality
-    Eyebrow shape
-    Eye size & spacing
-    Cheekbone prominence
-    Mouth
-    Jawline Definition
-    Neotenous Features
-    Skin clarity
-    Body shape ratio
-    Shoulder-to-waist ratio
-    Muscle tone/body composition
-    Secondary Sexual Characteristics
-    Ethnicity
-    Weight/Body Mass Index (BMI)
-    Perceived health
-    Overall grooming
-    Estimated Age
+- Facial symmetry
+- Golden Ratio/Facial Proportions
+- Hair quality
+- Eyebrow shape
+- Eye size & spacing
+- Cheekbone prominence
+- Mouth
+- Jawline Definition
+- Neotenous Features
+- Skin clarity
+- Body shape ratio
+- Shoulder-to-waist ratio
+- Muscle tone/body composition
+- Secondary Sexual Characteristics
+- Ethnicity
+- Weight/Body Mass Index (BMI)
+- Perceived health
+- Overall grooming
+- Estimated Age
 
 Only provide stats for features that are actually visible in the image. For example, if the person is wearing a hat, do not rate their hair quality. If they are wearing sunglasses, do not rate their eye size and spacing.
 
