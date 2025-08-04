fac18711b68a458b9be5fc2f18992bc088349080
Fortyseven <fortyseven@network47.org>
2025-08-03 22:27:04 -0400

    feat: restyled new config options

--- src/app/ChatSidebar/Config/ConfigPanel.svelte
+++ src/app/ChatSidebar/Config/ConfigPanel.svelte
@@ -16,7 +16,7 @@
 {#if open}
     <Modal title="Configuration Panel" {open}>
         <div class="form-group">
-            <div>
+            <div class="field">
                 <label title="Enable or disable audio feedback.">
                     Audio feedback
                     <input
@@ -25,13 +25,13 @@
                     />
                 </label>
             </div>
-            <div>
+            <div class="field">
                 <label title="Enable or disable the use of the titler.">
                     Use Titler
                     <input type="checkbox" bind:checked={$appState.useTitler} />
                 </label>
             </div>
-            <div>
+            <div class="field">
                 <label title="Resize images on paste">
                     Resize images for inference
                     <input
@@ -41,8 +41,8 @@
                 </label>
             </div>
         </div>
-        <div class="form-row">
-            <div class="form-group">
+        <div class="form-group">
+            <div class="field">
                 <label
                     for="ollama-api-endpoint"
                     title="The API endpoint for Ollama with a specific URL."
@@ -64,78 +64,90 @@
             </div>
         </div>
 
-        <div class="form-group">
-            <label
-                for="backpack-api-endpoint"
-                title="The API endpoint for Chit Backpack."
-                >Chit Backpack Endpoint (Enables enhanced mode.)
-                <input
-                    id="backpack-api-endpoint"
-                    type="text"
-                    value={$appState.backpackApiEndpoint}
-                    on:input={(e) =>
-                        appStateSetBackpackApiEndpoint(e.target.value)}
-                />
-            </label>
-        </div>
+        <!-- --------------------------------------------- -->
 
         <div class="form-group">
-            <label
-                for="default-prompt"
-                title="Prompt that populates the system prompt for all new sessions when not loaded from a preset."
-                >Default Prompt
-                <textarea
-                    id="default-prompt"
-                    rows="4"
-                    placeholder="Enter your default prompt here..."
-                    bind:value={$appState.defaultPrompt}
-                ></textarea>
-            </label>
+            <div class="field">
+                <label
+                    for="default-prompt"
+                    title="Prompt that populates the system prompt for all new sessions when not loaded from a preset."
+                    >Default Prompt
+                    <textarea
+                        id="default-prompt"
+                        rows="4"
+                        placeholder="Enter your default prompt here..."
+                        bind:value={$appState.defaultPrompt}
+                    ></textarea>
+                </label>
+            </div>
         </div>
 
-        <div class="form-group">
-            <label
-                for="default-ollama-model"
-                title="Default Ollama model to use for new conversations. Leave blank to use the built-in default."
-                >Default Ollama Model
-                <input
-                    id="default-ollama-model"
-                    type="text"
-                    placeholder="e.g. gemma3:12b, llama3, etc."
-                    value={$appState.defaultModel}
-                    on:input={(e) => appStateSetDefaultModel(e.target.value)}
-                />
-            </label>
+        <div class="form-group row">
+            <div class="field">
+                <label
+                    for="default-ollama-model"
+                    title="Default Ollama model to use for new conversations. Leave blank to use the built-in default."
+                    >Default Ollama Model
+                    <input
+                        id="default-ollama-model"
+                        type="text"
+                        placeholder="e.g. gemma3:12b, llama3, etc."
+                        value={$appState.defaultModel}
+                        on:input={(e) =>
+                            appStateSetDefaultModel(e.target.value)}
+                    />
+                </label>
+            </div>
+            <div class="field">
+                <label
+                    for="default-context-size"
+                    title="Default context size for new conversations. Leave blank to use the built-in default."
+                    >Default Context Size
+                    <input
+                        id="default-context-size"
+                        type="text"
+                        placeholder="e.g. 65536, 4096, etc."
+                        value={$appState.defaultContext}
+                        on:input={(e) =>
+                            appStateSetDefaultContext(e.target.value)}
+                    />
+                </label>
+            </div>
+            <div class="field">
+                <label
+                    for="default-temperature"
+                    title="Default temperature for new conversations. Leave blank to use the built-in default."
+                    >Default Temperature
+                    <input
+                        id="default-temperature"
+                        type="text"
+                        placeholder="e.g. 0.7, 1.0, etc."
+                        value={$appState.defaultTemperature}
+                        on:input={(e) =>
+                            appStateSetDefaultTemperature(e.target.value)}
+                    />
+                </label>
+            </div>
         </div>
 
-        <div class="form-group">
-            <label
-                for="default-context-size"
-                title="Default context size for new conversations. Leave blank to use the built-in default."
-                >Default Context Size
-                <input
-                    id="default-context-size"
-                    type="text"
-                    placeholder="e.g. 65536, 4096, etc."
-                    value={$appState.defaultContext}
-                    on:input={(e) => appStateSetDefaultContext(e.target.value)}
-                />
-            </label>
-        </div>
+        <hr />
+        <br />
 
         <div class="form-group">
-            <label
-                for="default-temperature"
-                title="Default temperature for new conversations. Leave blank to use the built-in default."
-                >Default Temperature
-                <input
-                    id="default-temperature"
-                    type="text"
-                    placeholder="e.g. 0.7, 1.0, etc."
-                    value={$appState.defaultTemperature}
-                    on:input={(e) => appStateSetDefaultTemperature(e.target.value)}
-                />
-            </label>
+            <div class="field">
+                <label
+                    for="backpack-api-endpoint"
+                    title="The API endpoint for Chit Backpack."
+                    >Chit Backpack Endpoint (Enables enhanced mode.)
+                    <input
+                        id="backpack-api-endpoint"
+                        type="text"
+                        value={$appState.backpackApiEndpoint}
+                        on:input={(e) =>
+                            appStateSetBackpackApiEndpoint(e.target.value)}
+                    />
+                </label>
+            </div>
         </div>
     </Modal>
 {/if}
@@ -150,39 +162,50 @@
             flex: 1;
         }
 
-        label {
-            display: block;
-            margin-bottom: 0.5rem;
-            font-weight: 500;
-            width: 100%;
-            color: var(--color-accent-lighter);
+        &.row {
+            display: grid;
+            grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+            gap: 0.5rem;
         }
 
-        input[type="text"] {
-            width: 100%;
-            padding: 0.5em;
-            border-radius: var(--border-radius-standard);
-            border: 1px solid #555;
-            background-color: #111;
-            color: var(--color-accent);
-            font-family: monospace;
-            box-sizing: border-box;
-        }
+        .field {
+            label {
+                display: block;
+                font-weight: 500;
+                width: 100%;
+                color: var(--color-accent-lighter);
+                input,
+                textarea {
+                    margin-top: 0.5rem;
+                }
 
-        textarea {
-            width: 100%;
-            padding: 0.5em;
-            border-radius: var(--border-radius-standard);
-            border: 1px solid #555;
-            background-color: #111;
-            color: var(--color-accent);
-            resize: vertical;
-            box-sizing: border-box;
-            font-family: monospace;
-        }
+                input[type="text"] {
+                    width: 100%;
+                    padding: 0.5em;
+                    border-radius: var(--border-radius-standard);
+                    border: 1px solid #555;
+                    background-color: #111;
+                    color: var(--color-accent);
+                    font-family: monospace;
+                    box-sizing: border-box;
+                }
 
-        input[type="checkbox"] {
-            margin-right: 0.5rem;
+                textarea {
+                    width: 100%;
+                    padding: 0.5em;
+                    border-radius: var(--border-radius-standard);
+                    border: 1px solid #555;
+                    background-color: #111;
+                    color: var(--color-accent);
+                    resize: vertical;
+                    box-sizing: border-box;
+                    font-family: monospace;
+                }
+
+                input[type="checkbox"] {
+                    margin-right: 0.5rem;
+                }
+            }
         }
     }
 </style>
