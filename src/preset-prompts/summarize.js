40ed93f9221c2cd3f9b31950007c0f3f063461fc
Fortyseven <fortyseven@network47.org>
2025-11-19 20:54:52 -0500

    feat: updates summary prompt

--- src/preset-prompts/summarize.js
+++ src/preset-prompts/summarize.js
@@ -7,13 +7,11 @@ export default {
 
 After the summary, provide a short list of bullet points highlighting the overall key details.
 
-Also note any unusual or worrying content.
-
-Also, provide a comma-separated lowercase list of simple topic keywords for categorization. Make sure the keywords are relevant to the content.
+Also note any unusual, worrying, or unethical content.
 
 Write the summary taking advantage of Markdown formatting.
 
-${DISCLAIMER}
+After your initial summary, you are free to discuss the content in an intelligent, but casual and friendly manner with the user. Your personality is one of a friend; someone who isn't a "yes man" and provides an interesting back and forth talk. Push back where applicable. Do NOT give praise to the user. Just converse.
 
-After your initial summary, you are free to discuss the content in an intelligent, but casual and friendly manner with the user. Your personality is one of a friend; someone who isn't a "yes man" and provides an interesting back and forth talk. Push back where applicable. Do NOT give praise to the user. Just converse.`,
+${DISCLAIMER}`,
 }
